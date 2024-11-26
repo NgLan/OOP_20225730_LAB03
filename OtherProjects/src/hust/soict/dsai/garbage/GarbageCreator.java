703931207935b24954e6c43817ffdd66eb4e2391
NgLan <ngoclan271204@gmail.com>
2024-11-26 19:48:26 +0700

    fix class GarbageCreator and class NoGarbage

--- OtherProjects/src/hust/soict/dsai/garbage/GarbageCreator.java
+++ OtherProjects/src/hust/soict/dsai/garbage/GarbageCreator.java
@@ -1,26 +1,25 @@
 package hust.soict.dsai.garbage;
 
-import java.io.File;  
-import java.io.FileNotFoundException;
-import java.util.Scanner;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 public class GarbageCreator {
 
-	public static void main(String[] args) {
-		try {
-	      File f = new File("D:\\OOP_20225730_LAB03\\OtherProjects\\src\\hust\\soict\\dsai\\garbage\\test.txt");
-	      Scanner reader = new Scanner(f);
-	      String data = "";
-	      long start = System.currentTimeMillis();
-	      while (reader.hasNextLine()) {
-	        data += reader.nextLine();
-	      }
-	      System.out.println(System.currentTimeMillis() - start);
-	      reader.close();
-	    } catch (FileNotFoundException e) {
-	      System.out.println("An error occurred.");
-	      e.printStackTrace();
-	    }
+	public static void main(String[] args) throws IOException {
+		String filename = "src\\hust\\soict\\dsai\\garbage\\test.exe"; 
+		byte[] inputBytes = { 0 };
+		long startTime, endTime;
+		
+		inputBytes = Files.readAllBytes(Paths.get(filename));
+		
+		startTime = System.currentTimeMillis();
+		String outputString = "";
+		for (byte b : inputBytes) {
+			outputString += (char)b;
+		}
+		endTime = System.currentTimeMillis();
+		System.out.println(endTime - startTime);
 	}
 	
 }
--- OtherProjects/src/hust/soict/dsai/garbage/NoGarbage.java
+++ OtherProjects/src/hust/soict/dsai/garbage/NoGarbage.java
@@ -1,26 +1,25 @@
 package hust.soict.dsai.garbage;
 
-import java.io.File;  
-import java.io.FileNotFoundException;
-import java.util.Scanner;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 public class NoGarbage {
 
-	public static void main(String[] args) {
-		try {
-			File f = new File("D:\\OOP_20225730_LAB03\\OtherProjects\\src\\hust\\soict\\dsai\\garbage\\test.txt");
-			Scanner reader = new Scanner(f);
-			StringBuilder data = new StringBuilder();
-			long start = System.currentTimeMillis();
-			while (reader.hasNextLine()) {
-				data.append(reader.nextLine()).append('\n');
-			}
-			System.out.println(System.currentTimeMillis() - start);
-			reader.close();
-	    } catch (FileNotFoundException e) {
-	        System.out.println("An error occurred.");
-	        e.printStackTrace();
-	    }
+	public static void main(String[] args) throws IOException {
+		String filename = "src\\hust\\soict\\dsai\\garbage\\test.exe"; 
+		byte[] inputBytes = { 0 };
+		long startTime, endTime;
+		
+		inputBytes = Files.readAllBytes(Paths.get(filename));
+		
+		startTime = System.currentTimeMillis();
+		StringBuilder outputStringBuilder = new StringBuilder();
+		for (byte b : inputBytes) {
+			outputStringBuilder.append((char)b);
+		}
+		endTime = System.currentTimeMillis();
+		System.out.println(endTime - startTime);
 	}
 
 }
