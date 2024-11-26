30b9d8625118e78a9a5ad5ecaf5f3665bdc72766
NgLan <ngoclan271204@gmail.com>
2024-11-26 11:03:36 +0700

    Add StoreTest class

---
+++ AimsProject/src/StoreTest.java
@@ -0,0 +1,25 @@
+public class StoreTest {
+	public static void main(String[] args) {
+		Store store = new Store();
+		
+		DigitalVideoDisc dvd1 = new DigitalVideoDisc("The Lion King", 
+                "Animation", "Roger Allers", 87, 19.95f);
+		store.addDVD(dvd1);
+
+        DigitalVideoDisc dvd2 = new DigitalVideoDisc("Star Wars", 
+                "Science Fiction", "George Lucas", 87, 24.95f);
+        store.addDVD(dvd2);
+
+        DigitalVideoDisc dvd3 = new DigitalVideoDisc("Aladin", 
+                "Animation", 18.99f);
+        store.addDVD(dvd3);	
+        
+        //Store before remove 
+        store.print();
+        System.out.println();
+        
+        store.removeDVD(dvd2);
+        //Store after remove
+        store.print();
+	}
+}
