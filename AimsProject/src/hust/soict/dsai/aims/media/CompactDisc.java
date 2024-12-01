2f2304f392306a523dced5d2ac7c58832f2b5cd7
NgLan <ngoclan271204@gmail.com>
2024-12-01 13:41:42 +0700

    Add 2 fields to CompactDisc class

--- AimsProject/src/hust/soict/dsai/aims/media/CompactDisc.java
+++ AimsProject/src/hust/soict/dsai/aims/media/CompactDisc.java
@@ -10,4 +10,36 @@ public class CompactDisc extends Disc {
 		// TODO Auto-generated constructor stub
 	}
 
+	public CompactDisc(int id, String title, String category, String director, int length, float cost, String artist,
+			ArrayList<Track> tracks) {
+		super(id, title, category, director, length, cost);
+		this.artist = artist;
+		this.tracks = tracks;
+	}
+
+	public String getArtist() {
+		return artist;
+	}
+	
+	public int getLength() {
+		int CDLength = 0;
+		for (Track trk : tracks) {
+			CDLength += trk.getLength();
+		}
+		return CDLength;
+	}
+
+	public void addTrack(Track trk) {
+		if (tracks.contains(trk)) 
+			System.out.println(trk.getTitle() + " is already exists");
+		else 
+			tracks.add(trk);
+	}
+	
+	public void removeTrack(Track trk) {
+		if (tracks.contains(trk)) 
+			tracks.remove(trk);
+		else 
+			System.out.println(trk.getTitle() + " is not exists");
+	}
 }
